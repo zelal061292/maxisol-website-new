3fb4a1c5e146fc2a3705c23d91013b497148880c
zelal061292 <81559927+zelal061292@users.noreply.github.com>
2021-06-15 23:48:08 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :posts, only: [ :index, new, :create, :delete, :show, :update]
 end
