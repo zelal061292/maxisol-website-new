bcc485754303c6a315672f5bcc91dff24bc003e6
Zelal Aslan <zelalaslan2@gmail.com>
2021-06-18 17:08:27 +0200

    rootes to the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  # root to: 'pages#home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :posts, only: [ :index, :new, :create, :delete, :show, :update]
+
+  #unauthenticated user do
+  unauthenticated :user do
+    root to: "pages#home", as: "home"
+  end
+
+  #authenticate user do
+  root to: "pages#dashboard", as:"dashboard"
+
+  # get 'users/:id/dashboard', to: 'users#dashboard', as: :user_dashboard
 
 end
