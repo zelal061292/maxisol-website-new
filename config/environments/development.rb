4b4f388a8984792b2db20a46c1312d96da690382
Zelal Aslan <zelalaslan2@gmail.com>
2021-07-02 20:22:12 +0200

    contact mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,20 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      domain: 'gmail.com',
+      port: 587,
+      user_name: 'rge@maxisol.fr',
+      password: ENV["GMAIL_KEY"],
+      authentication: 'plain',
+      enable_starttls_auto: true
+    }
+
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
 
